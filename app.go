7785aee85c20984c9e6daeb0879e20bfdfc06dc1
KuroNeko <kuroneko@ltra.cc>
2024-04-20 23:40:34 +0900

    possible to set deploy path and apply goroutine

--- app.go
+++ app.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 
+	"github.com/akamensky/argparse"
 	"github.com/go-yaml/yaml"
 	"github.com/melbahja/goph"
+	"github.com/rs/zerolog"
 )
 
 type KeployCompose struct {
@@ -22,22 +26,45 @@ type KeployCompose struct {
 	Cmds     []string
 }
 
-func main() {
-	compose := KeployCompose{}
-	data, err := ioutil.ReadFile(os.Args[1])
+func GetLogger() zerolog.Logger {
+	logfile, err := os.OpenFile("deploy.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+	}
+	return zerolog.New(
+		zerolog.ConsoleWriter{Out: logfile, TimeFormat: time.RFC3339},
+	).Level(zerolog.TraceLevel).With().Timestamp().Caller().Logger()
+}
+
+func Deploy(dir string) bool {
+	os.Chdir(dir)
+	logger := GetLogger()
+	logger.Info().Msg("Entered " + dir)
+
+	compose := KeployCompose{}
+	cwd, err := os.Getwd()
+	if err != nil {
+		logger.Err(err)
+		return false
+	}
+
+	data, err := ioutil.ReadFile("deploy.yml")
+	if err != nil {
+		logger.Err(err)
+		return false
 	}
 	yaml.Unmarshal(data, &compose)
 
 	auth, err := goph.Key(compose.KeyFile, compose.KeyPass)
 	if err != nil {
+		logger.Err(err)
 		auth = goph.Password(compose.Password)
 	}
 
 	callback, err := goph.DefaultKnownHosts()
 	if err != nil {
-		log.Fatal(err)
+		logger.Err(err)
+		return false
 	}
 
 	client, err := goph.NewConn(&goph.Config{
@@ -49,31 +76,93 @@ func main() {
 		Callback: callback,
 	})
 	if err != nil {
-		log.Fatal(err)
+		logger.Err(err)
+		return false
 	}
 	defer client.Close()
 
-	for _, item := range compose.Files {
+	for i, item := range compose.Files {
 		tmp := strings.Split(item, ":")
 		if len(tmp) != 2 {
-			log.Fatal("invalid files string")
+			logger.Error().Msg("invalid files string. Ignore " + item)
+			continue
 		}
 		src, dst := tmp[0], tmp[1]
+		logger.Info().Msg("Upload[" + strconv.Itoa(i) + "] " + src + " -> " + dst)
 		err := client.Upload(src, dst)
 		if err != nil {
-			log.Fatal(err)
+			logger.Err(err)
+			return false
 		}
+		logger.Info().Msg("Upload[" + strconv.Itoa(i) + "] Success")
 	}
 
 	for _, cmd := range compose.Cmds {
-		fmt.Println(cmd)
-		xmd, err := client.Command("bash", "-c", cmd)
+		logger.Info().Msg("CMD: " + cmd)
+		res, err := client.Run(cmd)
 		if err != nil {
-			log.Fatal(err)
+			logger.Err(err)
+			continue
 		}
-		err = xmd.Run()
-		if err != nil {
-			log.Fatal(err)
+		logger.Info().Msg("RES: " + string(res))
+	}
+
+	os.Chdir(cwd)
+	logger.Info().Msg("Leaved to " + cwd)
+	return true
+}
+
+func Worker(deploy_paths <-chan string, deploy_result chan<- bool) {
+	for deploy_path := range deploy_paths {
+		res := Deploy(deploy_path)
+		deploy_result <- res
+	}
+}
+
+func main() {
+	logger := zerolog.New(
+		zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339},
+	).Level(zerolog.TraceLevel).With().Timestamp().Caller().Logger()
+
+	parser := argparse.NewParser("keploy", "deploy system using ssh")
+	dir := parser.String("d", "dir", &argparse.Options{Required: true, Help: "deploy folder"})
+	err := parser.Parse(os.Args)
+	if err != nil {
+		logger.Err(err)
+		os.Exit(1)
+	}
+
+	const nJobs = 5
+	deploy_paths := make(chan string, nJobs)
+	deploy_result := make(chan bool)
+
+	for i := 0; i < nJobs; i++ {
+		go Worker(deploy_paths, deploy_result)
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		logger.Err(err)
+		os.Exit(1)
+	}
+	entrys, err := os.ReadDir(*dir)
+	if err != nil {
+		logger.Err(err)
+		os.Exit(1)
+	}
+
+	nResult := 0
+	for _, entry := range entrys {
+		if entry.IsDir() {
+			deploy_path := filepath.Join(cwd, *dir, entry.Name())
+			logger.Info().Msg("Start " + deploy_path)
+			deploy_paths <- deploy_path
+			nResult += 1
 		}
+	}
+	close(deploy_paths)
+
+	for i := 0; i < nResult; i++ {
+		<-deploy_result
 	}
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module keploy
 go 1.18
 
 require (
+	github.com/akamensky/argparse v1.4.0 // indirect
 	github.com/go-yaml/yaml v2.1.0+incompatible // indirect
 	github.com/kr/fs v0.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/melbahja/goph v1.4.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pkg/sftp v1.13.5 // indirect
+	github.com/rs/zerolog v1.32.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
